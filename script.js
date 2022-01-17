da15832bf2b77d1f6be70e26ca7d8760daf93f21
Robert Vilhelmsson <vilhelmsson.robert@gmail.com>
2022-01-17 07:42:16 +0100

    Turned it in to best of five

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const choices = ['rock', 'paper', 'scissors'];
 const beats = {'rock': 'paper', 'paper': 'scissors', 'scissorcs': 'rock'};
 const upper = {'rock': 'Rock', 'paper': 'Paper', 'scissors': 'Scissors'};
 
+// Computers choice
 function computerPlay () {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+// The game being played
 function playRound(human, computer) {
     return human === computer
         ? `Oh! It's a tie`
@@ -13,8 +15,33 @@ function playRound(human, computer) {
         ? `You win! ${upper[human]} beats ${computer}`
         : `You lose! ${upper[computer]} beats ${human}`;
 }
-    
-const playerSelection = choices[0];
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
 
+let humanScore = parseInt(0);
+let computerScore = parseInt(0);
+
+
+var i = 0;
+const play = () => {
+    let human = prompt("Rock, Paper or Scissors? Choose your weapon!").toLowerCase();
+    let computer = computerPlay();
+    let roundResult = playRound(human, computer);
+
+    if (roundResult.includes('win')) {
+        humanScore++;
+    } else if (roundResult.includes('lose')) {
+        computerScore++;
+    }
+
+    console.log(playRound(human, computer))
+    console.log("your score = " + humanScore);
+    console.log("Computer's score = " + computerScore);
+    i++;
+    if (i !== 5) {
+        play();
+    } else {
+        alert("Game Over=> User("+humanScore+") vs Computer("+computerScore+")");
+    }
+}
+   
+
+play();
